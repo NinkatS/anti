8208a0479bd847e8eedf6e4db421786790116f19
Lee Yeonggeun <dudrms9397@naver.com>
2023-11-11 12:08:46 +0900

    login

--- src/main/java/com/example/anti/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.anti.repository;
-
-import com.example.anti.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findByUsername(String username);
-}
