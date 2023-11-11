90dda6f7ac315a19a13b746e8aa1f1732b6317de
hh Song <ssongh2h@gmail.com>
2023-11-11 09:35:27 +0900

    front

--- anti/src/App.js
+++ anti/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </div>
+    </Router>
+    <Footer/>
+    </>
+
   );
 }
 
---
+++ anti/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React, { useState,useContext } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {useNavigate} from "react-router-dom"
+import axios from "axios"
+
+export default function Login(){
+    return(
+        <>
+        asd
+        </>
+    )
+}
